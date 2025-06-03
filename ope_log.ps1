27ad5a83799077b41f490af3a9967da737d30677
Szymni <283863@student.pwr.edu.pl>
2025-06-03 22:18:33 +0200

    Skrypt sprawdzający poprawność logowania

---
+++ ope_log.ps1
@@ -0,0 +1,13 @@
+﻿$login = Read-Host "Podaj nazwe uzytkownika: " 
+$haslo_maska = Read-Host "Podaj haslo: " -AsSecureString
+
+$haslo = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
+    [Runtime.InteropServices.Marshal]::SecureStringToBSTR($haslo_maska)
+)
+
+
+if ($login -eq "admin" -and $haslo -eq "password") {
+    Write-Host "Poprawne logowanie!"
+} else {
+    Write-Host "Blad logowania"
+}
