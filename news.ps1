6fc5fe3a8db99336cfdbad9b6b5d559b2dbc60fe
Szymni <283863@student.pwr.edu.pl>
2025-06-03 22:19:41 +0200

    Skrypt wyświetljący najnowsze wiadomości ze świata

---
+++ news.ps1
@@ -0,0 +1,16 @@
+﻿param (
+    [Parameter(Mandatory=$true)]
+    [string]$Temat
+)
+
+$apiKey = "69b430e1071947bd9b068d6baf8adc35"
+$url = "https://newsapi.org/v2/everything?q=$Temat&pageSize=5&language=pl&apiKey=$apiKey"
+
+$response = Invoke-RestMethod -Uri $url
+
+$response.articles | ForEach-Object {
+    Write-Host "`nTytuł: $($_.title)"
+    Write-Host "Źródło: $($_.source.name)"
+    Write-Host "Link: $($_.url)"
+    Write-Host "Data: $($_.publishedAt)"
+}
