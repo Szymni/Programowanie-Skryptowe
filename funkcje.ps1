df10c6dd58429019ac6c1dcaff45e12aee0bd6f7
Szymni <283863@student.pwr.edu.pl>
2025-06-03 22:23:41 +0200

    Skrypt podający: datę, wersje systemu, nazwę bieżącego użytkownika i adres IP

---
+++ funkcje.ps1
@@ -0,0 +1,25 @@
+﻿$komputer = $Env:COMPUTERNAME
+ 
+Function Aktualna-Data {
+    Write-Host "Aktualna data na $komputer to: $(Get-Date)"
+}
+
+Function Wersja-Systemu {
+    $nazwa = (Get-ComputerInfo).OsName
+    $wersja = (Get-ComputerInfo).OsVersion
+    Write-Host "Aktualna nazwa i wersja systemu na $komputer to: $nazwa, $wersja"
+}
+
+Function Uzytkownik {
+    $user = $Env:USERNAME
+    Write-Host "Aktualny użytkownik na $komputer to: $user"
+}
+
+function Adres-IP {
+    $ip = (Test-Connection -ComputerName $env:COMPUTERNAME -Count 1).IPV4Address.IPAddressToString
+    Write-Host "Adres IP na $komputer to $ip"
+}
+Aktualna-Data
+Wersja-Systemu
+Uzytkownik
+Adres-IP
