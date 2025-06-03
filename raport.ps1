1d6c794eef689b559f4324d21e26d6fea3e161e9
Szymni <283863@student.pwr.edu.pl>
2025-06-03 22:17:48 +0200

    Skrypt robiący raport o sprzęcie

---
+++ raport.ps1
@@ -0,0 +1,21 @@
+﻿$components = Import-Csv "C:\Users\szymk\Desktop\Programowanie\skyptowe\input_file.csv" ";"
+
+$info = Get-ComputerInfo
+$serial = (Get-WmiObject -Class Win32_BIOS).SerialNumber
+$cpuName = (Get-WmiObject Win32_Processor).Name
+$ram = (Get-CimInstance -ClassName Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum / 1GB
+$ramFormatted = "{0:N0} GB" -f $ram
+
+foreach ($item in $components) {
+    if ($item.GenerateReport -eq 'True') {
+        switch ($item.Component) {
+            "Computername"   { Write-Host "Computername: $env:COMPUTERNAME" }
+            "Manufacturer"   { Write-Host "Manufacturer: $($info.CsManufacturer)" }
+            "Model"          { Write-Host "Model: $($info.CsModel)" }
+            "SerialNumber"   { Write-Host "SerialNumber: $serial" }
+            "CpuName"        { Write-Host "CpuName: $cpuName" }
+            "RAM"            { Write-Host "RAM: $ramFormatted" }
+            default          { Write-Host "$($item.Component): Nieobsługiwany komponent" }
+        }
+    }
+}
