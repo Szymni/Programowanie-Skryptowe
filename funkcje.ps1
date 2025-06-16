7750e7a3f42d610cf44638986cca0c236f98ef99
Szymni <283863@student.pwr.edu.pl>
2025-06-16 09:32:45 +0200

    Poprawka funkcji w powershellu

--- funkcje.ps1
+++ funkcje.ps1
@@ -19,7 +19,35 @@ function Adres-IP {
     $ip = (Test-Connection -ComputerName $env:COMPUTERNAME -Count 1).IPV4Address.IPAddressToString
     Write-Host "Adres IP na $komputer to $ip"
 }
+
+function RAM-Info {
+    $mem = Get-CimInstance Win32_OperatingSystem
+    $total = [math]::Round($mem.TotalVisibleMemorySize / 1MB, 2)
+    Write-Output "Dostępna pamięć RAM: $total GB"
+}
+
+function Disk-Usage {
+    $disk = Get-CimInstance Win32_LogicalDisk -Filter "DeviceID='C:'"
+    $size = [math]::Round($disk.Size / 1GB, 2)
+    $free = [math]::Round($disk.FreeSpace / 1GB, 2)
+    $used = [math]::Round($size - $free, 2)
+    Write-Output "Dysk C: zużyte $used GB z $size GB"
+}
+
+function GEt-MAC {
+    $mac = Get-NetAdapter | Where-Object { $_.Status -eq 'Up'} | Select-Object -First 1 -ExpandProperty MacAddress
+    Write-Output "Adres MAC: $mac"
+}
+
+function Host-Name {
+  Write-Output "Nazwa hosta: $($env:COMPUTERNAME)"
+}
+
 Aktualna-Data
 Wersja-Systemu
 Uzytkownik
 Adres-IP
+RAM-Info
+Disk-Usage
+GEt-MAC
+Host-Name
